fa6b5b50376a9631d92c2235a7c7c6c2981fecba
Kittiphat <kittiphat.rpp@gmail.com>
2025-03-17 01:29:58 +0700

    feat(ui): Create article section and add hover button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import ArticleSection from "./components/ArticleSection";
 import Footer from "./components/Footer";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <>
       <Navbar />
       <HeroSection />
+      <ArticleSection />
       <Footer />
     </>
   );
